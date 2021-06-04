bbd4d7b81020a2cb150e44a27443e6200c9ebe17
蒋子航 <32448880+zihangJiang@users.noreply.github.com>
2021-06-04 16:10:56 +0800

    Update pretrained models

--- eval.sh
+++ eval.sh
@@ -10,27 +10,15 @@ then
 else
     MODEL_DIR="$2"
 fi
-python3 validate.py $DATA_DIR  --model lvvit_s --checkpoint $MODEL_DIR/lvvit_s-224-83.3.pth.tar --no-test-pool --amp --img-size 224 -b 64
+python3 validate.py $DATA_DIR  --model lvvit_s --checkpoint $MODEL_DIR/lvvit_s-26M-224-83.3.pth.tar --no-test-pool --amp --img-size 224 -b 64
 
-# python3 validate.py $DATA_DIR  --model lvvit_s --checkpoint $MODEL_DIR/lvvit_s-26M-384-84.4.tar --no-test-pool --amp --img-size 384 -b 64
+# python3 validate.py $DATA_DIR  --model lvvit_m --checkpoint $MODEL_DIR/lvvit_m-56M-224-84.0.pth.tar --no-test-pool --amp --img-size 224 -b 64
 
-# python3 validate.py $DATA_DIR  --model lvvit_m --checkpoint $MODEL_DIR/lvvit_m-56M-224-84.0.tar --no-test-pool --amp --img-size 224 -b 64
-
-# python3 validate.py $DATA_DIR  --model lvvit_m --checkpoint $MODEL_DIR/lvvit_m-56M-384-85.4.tar --no-test-pool --amp --img-size 384 -b 64
-
-# python3 validate.py $DATA_DIR  --model lvvit_m --checkpoint $MODEL_DIR/lvvit_m-56M-448-85.5.tar --no-test-pool --amp --img-size 448 -b 64
-
-# python3 validate.py $DATA_DIR  --model lvvit_l --checkpoint $MODEL_DIR/lvvit_l-150M-448-86.2.tar --no-test-pool --amp --img-size 448 -b 64
+# python3 validate.py $DATA_DIR  --model lvvit_l --checkpoint $MODEL_DIR/lvvit_l-150M-448-86.2.pth.tar --no-test-pool --amp --img-size 448 -b 64
 
 
-# python3 validate.py $DATA_DIR  --model lvvit_s --checkpoint $MODEL_DIR/lvvit_s-224-83.3.pth.tar --no-test-pool --amp --img-size 224 -b 64 --real-labels real.json
+# python3 validate.py $DATA_DIR  --model lvvit_s --checkpoint $MODEL_DIR/lvvit_s-26M-224-83.3.pth.tar --no-test-pool --amp --img-size 224 -b 64 --real-labels real.json
 
-# python3 validate.py $DATA_DIR  --model lvvit_s --checkpoint $MODEL_DIR/lvvit_s-26M-384-84.4.tar --no-test-pool --amp --img-size 384 -b 64 --real-labels real.json
+# python3 validate.py $DATA_DIR  --model lvvit_m --checkpoint $MODEL_DIR/lvvit_m-56M-224-84.0.pth.tar --no-test-pool --amp --img-size 224 -b 64 --real-labels real.json
 
-# python3 validate.py $DATA_DIR  --model lvvit_m --checkpoint $MODEL_DIR/lvvit_m-56M-224-84.0.tar --no-test-pool --amp --img-size 224 -b 64 --real-labels real.json
-
-# python3 validate.py $DATA_DIR  --model lvvit_m --checkpoint $MODEL_DIR/lvvit_m-56M-384-85.4.tar --no-test-pool --amp --img-size 384 -b 64 --real-labels real.json
-
-# python3 validate.py $DATA_DIR  --model lvvit_m --checkpoint $MODEL_DIR/lvvit_m-56M-448-85.5.tar --no-test-pool --amp --img-size 448 -b 64 --real-labels real.json
-
-# python3 validate.py $DATA_DIR  --model lvvit_l --checkpoint $MODEL_DIR/lvvit_l-150M-448-86.2.tar --no-test-pool --amp --img-size 448 -b 64 --real-labels real.json
+# python3 validate.py $DATA_DIR  --model lvvit_l --checkpoint $MODEL_DIR/lvvit_l-150M-448-86.2.pth.tar --no-test-pool --amp --img-size 448 -b 64 --real-labels real.json
