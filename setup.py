5cc1461d0a07bc616f6b866313c2261dade44acc
蒋子航 <32448880+zihangJiang@users.noreply.github.com>
2021-07-21 14:38:47 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
 from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
   name = 'tlt',
   packages = find_packages(exclude=['seg','visualize']),
-  version = '0.1.1',
+  version = '0.2.0',
   license='Apache License 2.0',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
   description = 'Token Labeling Toolbox for training image models',
   author = 'Zihang Jiang',
   author_email = 'jzh0103@gmail.com',
